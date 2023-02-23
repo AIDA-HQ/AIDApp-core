69319a2505e85a70d10cc53d7d0f1598d762cae0
Filippo Roggi <zzkw35@gmail.com>
2023-02-23 23:43:29 +0100

    Move GUI strings to its own module and remove some widgets
    * Adjust translations accordingly.
    
    * Remove the optional "manual input" widgets (too many redundant options may confuse the user).

--- src/translations/aidapp_it.ts
+++ src/translations/aidapp_it.ts
@@ -1,152 +1,132 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="it_IT" sourcelanguage="en">
-<context>
+  <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../gui.py" line="547"/>
-        <source>AIDApp</source>
-        <translation>AIDApp</translation>
+      <location filename="../strings.py" line="8" />
+      <source>AIDApp</source>
+      <translation>AIDApp</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="550"/>
-        <source>Input Values</source>
-        <translation>Valori di Input</translation>
+      <location filename="../strings.py" line="11" />
+      <source>Input Values</source>
+      <translation>Valori di Input</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="553"/>
-        <source>Zonation Data</source>
-        <translation>Dati Zonazione</translation>
+      <location filename="../strings.py" line="14" />
+      <source>Zonation Data</source>
+      <translation>Dati Zonazione</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="556"/>
-        <source>Enter seismic zonation values</source>
-        <translation>Inserire i valori di zonazione sismica</translation>
+      <location filename="../strings.py" line="17" />
+      <source>Enter seismic zonation values</source>
+      <translation>Inserire i valori di zonazione sismica</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="561"/>
-        <source>X Pushover</source>
-        <translation>X Pushover</translation>
+      <location filename="../strings.py" line="20" />
+      <source>X Pushover</source>
+      <translation>X Pushover</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="564"/>
-        <source>Enter pushover X coordinates</source>
-        <translation>Inserire le coordinate X della pushover</translation>
+      <location filename="../strings.py" line="23" />
+      <source>Enter pushover X coordinates</source>
+      <translation>Inserire le coordinate X della pushover</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="569"/>
-        <source>Y Pushover</source>
-        <translation>Y Pushover</translation>
+      <location filename="../strings.py" line="26" />
+      <source>Y Pushover</source>
+      <translation>Y Pushover</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="572"/>
-        <source>Enter pushover Y coordinates</source>
-        <translation>Inserire le coordinate Y della pushover</translation>
+      <location filename="../strings.py" line="29" />
+      <source>Enter pushover Y coordinates</source>
+      <translation>Inserire le coordinate Y della pushover</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="577"/>
-        <source>Damping coefficient [%]</source>
-        <translation>Coefficiente di smorzamento [%]</translation>
+      <location filename="../strings.py" line="32" />
+      <source>Damping coefficient [%]</source>
+      <translation>Coefficiente di smorzamento [%]</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="580"/>
-        <source>Nominal life [years]</source>
-        <translation>Vita nominale [anni]</translation>
+      <location filename="../strings.py" line="35" />
+      <source>Nominal life [years]</source>
+      <translation>Vita nominale [anni]</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="583"/>
-        <source>Functional class</source>
-        <translation>Classe d&apos;uso</translation>
+      <location filename="../strings.py" line="38" />
+      <source>Functional class</source>
+      <translation>Classe d'uso</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="586"/>
-        <source>Topographic factor</source>
-        <translation>Categoria topografica</translation>
+      <location filename="../strings.py" line="41" />
+      <source>Topographic factor</source>
+      <translation>Categoria topografica</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="589"/>
-        <source>Soil class</source>
-        <translation>Categoria di sottosuolo</translation>
+      <location filename="../strings.py" line="44" />
+      <source>Soil class</source>
+      <translation>Categoria di sottosuolo</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="592"/>
-        <source>Limit State Design</source>
-        <translation>Stato Limite di progetto</translation>
+      <location filename="../strings.py" line="47" />
+      <source>Limit State Design</source>
+      <translation>Stato Limite di progetto</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="595"/>
-        <source>d&lt;sub&gt;p&lt;/sub&gt; [m]</source>
-        <translation>d&lt;sub&gt;p&lt;/sub&gt; [m]</translation>
+      <location filename="../strings.py" line="50" />
+      <source>d&lt;sub&gt;p&lt;/sub&gt; [m]</source>
+      <translation>d&lt;sub&gt;p&lt;/sub&gt; [m]</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="598"/>
-        <source>μ&lt;sub&gt;DB&lt;/sub&gt;</source>
-        <translation>μ&lt;sub&gt;C&lt;/sub&gt;</translation>
+      <location filename="../strings.py" line="53" />
+      <source>μ&lt;sub&gt;DB&lt;/sub&gt;</source>
+      <translation>μ&lt;sub&gt;C&lt;/sub&gt;</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="601"/>
-        <source>κ&lt;sub&gt;DB&lt;/sub&gt;</source>
-        <translation>κ&lt;sub&gt;C&lt;/sub&gt;</translation>
+      <location filename="../strings.py" line="56" />
+      <source>κ&lt;sub&gt;DB&lt;/sub&gt;</source>
+      <translation>κ&lt;sub&gt;C&lt;/sub&gt;</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="604"/>
-        <source>κ&lt;sub&gt;(F)&lt;/sub&gt;</source>
-        <translation>κ&lt;sub&gt;(S)&lt;/sub&gt;</translation>
+      <location filename="../strings.py" line="59" />
+      <source>κ&lt;sub&gt;(F)&lt;/sub&gt;</source>
+      <translation>κ&lt;sub&gt;(S)&lt;/sub&gt;</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="607"/>
-        <source>Span Length [m]</source>
-        <translation>Lunghezza Campata [m]</translation>
+      <location filename="../strings.py" line="62" />
+      <source>Span Length [m]</source>
+      <translation>Lunghezza Campata [m]</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="610"/>
-        <source>Inter-floor Height [m]</source>
-        <translation>Altezza Interpiano [m]</translation>
+      <location filename="../strings.py" line="65" />
+      <source>Inter-floor Height [m]</source>
+      <translation>Altezza Interpiano [m]</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="613"/>
-        <source>Manual Input</source>
-        <translation>Input Manuale</translation>
+      <location filename="../strings.py" line="68" />
+      <source>Enter the mass of each storey</source>
+      <translation>Inserire la massa di ogni piano</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="616"/>
-        <source># of storeys:</source>
-        <translation>Numero di piani:</translation>
+      <location filename="../strings.py" line="71" />
+      <source>Enter the first eigenvalue</source>
+      <translation>Inserire i valori del primo autovettore</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="619"/>
-        <source>Send</source>
-        <translation>Invia</translation>
+      <location filename="../strings.py" line="74" />
+      <source>Enter the amount of upwinds of each storey</source>
+      <translation>Inserire il numero di controventi di ogni piano</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="622"/>
-        <source>Show Storey Data</source>
-        <translation>Mostra dati dei piani</translation>
+      <location filename="../strings.py" line="78" />
+      <source>Ok</source>
+      <translation>Ok</translation>
     </message>
     <message>
-        <location filename="../gui.py" line="624"/>
-        <source>Ok</source>
-        <translation>Ok</translation>
+      <location filename="../strings.py" line="80" />
+      <source>Output Values</source>
+      <translation>Valori di Output</translation>
     </message>
-    <message>
-        <location filename="../gui.py" line="626"/>
-        <source>Output Values</source>
-        <translation>Valori di Output</translation>
-    </message>
-    <message>
-        <location filename="../gui.py" line="650"/>
-        <source>Enter the mass of each storey</source>
-        <translation>Inserire la massa di ogni piano</translation>
-    </message>
-    <message>
-        <location filename="../gui.py" line="655"/>
-        <source>Enter the eigenvalue of each storey</source>
-        <translation>Inserire i valori del primo autovettore</translation>
-    </message>
-    <message>
-        <location filename="../gui.py" line="660"/>
-        <source>Enter the amount of upwinds of each storey</source>
-        <translation>Inserire il numero di controventi di ogni piano</translation>
-    </message>
-</context>
+  </context>
 </TS>
