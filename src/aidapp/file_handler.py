4b3d60d6e7e5f69d35122d63f6c909e90195920a
Filippo Roggi <zzkw35@gmail.com>
2023-08-03 21:18:35 +0200

    Remove leftover pieces of FE

--- src/aidapp/file_handler.py
+++ src/aidapp/file_handler.py
@@ -1,7 +1,6 @@
 """This module contains the functions that handle the input and output data."""
 
 from numpy import array, reshape, transpose
-from qtpy import QtWidgets
 from aidapp.utils import rd
 
 
@@ -39,23 +38,3 @@ def generate_storey_data(storey_input_data):
     data = filter(None, storey_input_data.splitlines())
     storey_data = [float(element.replace(",", ".")) for element in data]
     return storey_data
-
-
-def generate_output_file(kc_n_s_array_arg, fc_n_s_array_arg):
-    """
-    This function takes two arrays and generates a file containing
-    the values of the kc and Fc for each story.
-    """
-    name_dialog, _ = QtWidgets.QFileDialog.getSaveFileName(
-        caption="Save File", filter="Text Files(*.txt)"
-    )
-
-    kc_n_s_array = "kc,i,s array: \n"
-    for element in kc_n_s_array_arg:
-        kc_n_s_array += str(element) + "\n"
-    fc_n_s_array = "Fc,i,s array: \n"
-    for element in fc_n_s_array_arg:
-        fc_n_s_array += str(element) + "\n"
-    if name_dialog:
-        with open(name_dialog, mode="w", encoding="utf-8") as file:
-            file.write(kc_n_s_array + "\n" + fc_n_s_array)
