86ccdcb44cbd4a57d60208e1e0994ad8fb90c040
Filippo Roggi <zzkw35@gmail.com>
2023-08-04 20:20:58 +0200

    Add pre-commit workflow

--- src/aidapp/__init__.py
+++ src/aidapp/__init__.py
@@ -1,4 +1,5 @@
-from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+from importlib.metadata import PackageNotFoundError  # pragma: no cover
+from importlib.metadata import version
 
 try:
     # Change here if project is renamed and does not equal the package name
