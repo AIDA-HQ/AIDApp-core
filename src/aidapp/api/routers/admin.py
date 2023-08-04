d00290675b6c86cbeb5d0a066a6642d3d9b69a28
Filippo Roggi <zzkw35@gmail.com>
2023-08-04 23:55:56 +0200

    Test invalid payload

--- src/aidapp/api/routers/admin.py
+++ src/aidapp/api/routers/admin.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
 
 from aidapp.api.models.model import InputValues, OutputValues
 from aidapp.main import main
@@ -8,8 +9,9 @@ router = APIRouter()
 
 @router.post("/calculate/", response_model=OutputValues)
 async def calculate(input_values: InputValues):
+    """Endpoint to feed the inputs to the algorithm and return the output values."""
     try:
         aidapp_output = main(input_values)
     except Exception as e:
-        return {"message": f"{e}"}
-    return aidapp_output._asdict()
+        raise HTTPException(status_code=500, detail=f"Internal server error: {e}")
+    return JSONResponse(content=aidapp_output._asdict(), status_code=200)
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,13 +1,15 @@
 import json
 
+import pytest
 from fastapi.testclient import TestClient
 
 from aidapp.api import main
+from aidapp.api.models.model import InputValues
 
 client = TestClient(main.app)
 
 
-def test_calculate():
+def test_calculate_endpoint_with_valid_input():
     """Test the /calculate/ endpoint."""
     with open("tests/test_payload.json") as f:
         payload = json.load(f)
@@ -34,3 +36,33 @@ def test_calculate():
     assert response.json()["de_0"] == [0.148465762254, 4.838711036209]
     assert response.json()["de_n"] == [0.092350797764, 7.807520454074]
     assert response.json()["dp"] == 0.044429959868
+
+
+def test_calculate_endpoint_with_invalid_input():
+    with pytest.raises(ValueError):
+        input_values = InputValues(
+            dp=0.1,
+            mu_DB=6,
+            k_DB=1,
+            kf=0.66,
+            storey_masses=[
+                239.6789,
+                231.4220183,
+            ],
+            eigenvalues=[0.113530691, 0.326058618],
+            brace_number=[2, 2],
+            zonation_0=[0.1, "invalid"],
+            zonation_1=[0.1, 0.2],
+            zonation_2=[0.1, 0.2],
+            pushover_x=[0, 0.0015, 0.003, 0.0045],
+            pushover_y=[0, 55.59392236, 109.3670878, 163.8144485],
+            span_length=6,
+            interfloor_height=3,
+            nominal_age=50,
+            functional_class="I",
+            topographic_factor="T1",
+            soil_class="A",
+            limit_state="SLV",
+            damping_coeff=5,
+        )
+        client.post("/calculate/", json=input_values.model_dump_json())
