4ff5a31611dbb448d85b66304c6c62415073b398
Filippo Roggi <zzkw35@gmail.com>
2023-10-11 19:00:38 +0200

    Add CORS support

--- src/aidapp/api/main.py
+++ src/aidapp/api/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from aidapp.api.routers import admin
 
-app = FastAPI(docs_url=None)
+app = FastAPI()
 
+origins = ["http://localhost:5173", "https://aidapp-fe.vercel.app/"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(admin.router, prefix="", tags=["admin"])
