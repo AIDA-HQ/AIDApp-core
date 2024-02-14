c65e9471643f45ed8672dc939d0fa96f5568d81c
Filippo Roggi <zzkw35@gmail.com>
2024-02-14 01:01:13 +0100

    Add basic sentry.io integration

--- src/aidapp/api/main.py
+++ src/aidapp/api/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import sentry_sdk
 
 from aidapp.api.routers import admin
 
+sentry_sdk.init(
+    dsn="https://a31edd4f6f92c32659dc2309eff1b95b@o4506740561149952.ingest.sentry.io/4506740561346560",
+    enable_tracing=True,
+)
+
 app = FastAPI()
 
-origins = ["http://localhost:5173", "https://aidapp-fe.vercel.app/"]
+origins = ["http://localhost:5173", "https://aidapp-fe.vercel.app/", "https://app.aidapp.it/"]
 
 app.add_middleware(
     CORSMiddleware,
